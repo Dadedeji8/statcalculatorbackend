624715faf1fe3cef839d94c19bb9d2b122e8bc1f
Dadedeji8 <97545459+Dadedeji8@users.noreply.github.com>
2024-11-04 13:14:48 +0100

    Update user.js

--- routes/user.js
+++ routes/user.js
@@ -34,7 +34,7 @@ userRoute.post('/signup', async (req, res, next) => {
     const result = await newUser.save();
     res.status(201).json({ message: 'User created successfully', user: result });
   } catch (error) {
-    res.status(500).json({ message: 'Internal server error', error });
+    res.status(500).json({ message: 'Internal server error from signup', error });
   }
 });
 
@@ -54,7 +54,7 @@ userRoute.post('/login', async (req, res, next) => {
         "secretKey",  // Use environment variable for the secret key
         { expiresIn: '1h' }
       );
-      res.status(200).json({ message: 'Auth successful', token });
+      res.status(200).json({ message: 'Auth successful', token, email: user.email });
     } else {
       res.status(401).json({ message: 'Auth failed: Incorrect password' });
     }
