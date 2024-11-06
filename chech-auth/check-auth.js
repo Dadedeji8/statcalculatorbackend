9be1463e16fbc10e6898959a3a3bbb2abff017aa
Dadedeji8 <97545459+Dadedeji8@users.noreply.github.com>
2024-11-06 20:16:51 +0100

    Update check-auth.js

--- chech-auth/check-auth.js
+++ chech-auth/check-auth.js
@@ -1,7 +1,7 @@
 const jwt = require("jsonwebtoken");
 
 module.exports = (req, res, next) => {
-    const token = req.headers['Authorization'];
+    const token = req.headers['authorization'];
     // if (!token) {
     //     return res.status(401).json({ message: "Auth failed: Token not provided" });
     // }
