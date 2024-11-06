b3b3b9f293da5b0be28d2347f3beb8a1c040910c
Dadedeji8 <97545459+Dadedeji8@users.noreply.github.com>
2024-11-06 18:41:07 +0100

    Update check-auth.js

--- chech-auth/check-auth.js
+++ chech-auth/check-auth.js
@@ -1,14 +1,14 @@
 const jwt = require("jsonwebtoken");
 
 module.exports = (req, res, next) => {
-    const authHeader = req.headers['Authorization'];  // Correct header case
+    const token = req.headers['Authorization'];  // Directly access the token
 
-
-
-    const token = authHeader;  // Extract token from 'Bearer <token>'
+    if (!token) {
+        return res.status(401).json({ message: "Auth failed: Token not provided" });
+    }
 
     try {
-        const decoded = jwt.verify(token, "secretKey");  // Use environment variable for the secret key
+        const decoded = jwt.verify(token, "secretKey");
         req.user = decoded;  // Store decoded user info in request object
         next();  // Proceed to the next middleware
     } catch (error) {
